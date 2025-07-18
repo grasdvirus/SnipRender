8cc5f416cdae6a6a2223e12eb7cd3e772ed2c353
publix store <publixstore.exe@gmail.com>
2025-07-18 20:01:24 +0000

    l'erreur se présente toujours si il y'a une autre façon de la résoudre f

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,6 +19,7 @@ import 'prismjs/components/prism-javascript';
 import 'prismjs/components/prism-css';
 import 'prismjs/components/prism-markup';
 import 'prismjs/components/prism-jsx';
+import 'prismjs/themes/prism-okaidia.css';
 
 
 type Snippet = {
